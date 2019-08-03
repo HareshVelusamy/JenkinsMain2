0ec630e69d615100ff33bb0ae06ef0ba24f4d80d
Haresh <haresh.leo@gmail.com>
2019-08-03 19:53:48 +0530

    firstCommit

---
+++ src/sampleMain2/jenkinClass2.java
@@ -0,0 +1,11 @@
+package sampleMain2;
+
+public class jenkinClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jenkins 2");
+
+	}
+
+}
